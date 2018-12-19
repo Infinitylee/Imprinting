35d233240696ee79181cee366ca91579e7d43f0f
pythem li <pythem.li@cansemitech.com>
2018-12-19 10:18:04 +0800

    更新打包项目文件

---
+++ output/印迹-linux-armv7l/resources/app/src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import LayoutContent from './main/app/LayoutContent';
+import LayoutHeader from './main/app/LayoutHeader';
+import PropTypes from 'prop-types';
+import SiderMenu from './main/app/SiderMenu';
+import { Layout } from 'antd';
+import { connect } from 'react-redux';
+import { setLayoutWidth, setLayoutHeight } from './redux/actions/LayoutAction';
+import './web/css/Main.css';
+import './web/css/SiderMenu.css';
+const { Sider } = Layout;
+
+class App extends Component {
+
+  static defaultProps = { };
+  
+  constructor(props) {
+    super(props);
+    this.state = { };
+  };
+
+  componentWillMount() {
+    // 窗口加载时初始化高度
+    this.props.setLayoutWidth(document.body.clientWidth);
+    this.props.setLayoutHeight(document.body.clientHeight);
+    // 注册窗口变化事件
+    window.onresize = () => {
+      this.props.setLayoutWidth(document.body.clientWidth);      
+      this.props.setLayoutHeight(document.body.clientHeight);
+    }
+  }
+
+  render() {
+    return (
+      <Layout style={{height: '100%'}}>
+        <Layout style={{height:'100%'}}>
+          <Sider
+            className="siderMenu"
+            style={{ zIndex: 1, height: '100%', position: 'fixed', left: 0 }}
+            breakpoint="lg"
+            collapsedWidth="0"
+            collapsed={false} // 是否折叠 true:折叠 false:不折叠(换用display解决隐藏时,关闭tab后再显示中午子菜单bug)
+          >
+            <SiderMenu />
+          </Sider>
+          <Layout style={{ height: '100%', marginLeft: '200px'}}>
+            <LayoutContent />
+          </Layout>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+App.contextTypes = {
+  router: PropTypes.object.isRequired
+};
+
+// mapStateToProps:将state映射到组件的props中
+const mapStateToProps = (state) => {
+  return {
+    height: state.layout.height,
+    width: state.layout.width,
+  }
+}
+
+// mapDispatchToProps:将dispatch映射到组件的props中
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    setLayoutWidth (width) {
+      dispatch(setLayoutWidth(width))
+    },
+    setLayoutHeight (height) {
+      dispatch(setLayoutHeight(height))
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
